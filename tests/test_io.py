14210214b5f69983491ecb608ffc9a1f616ab132
Alejandro Pérez Sanjuán <aperez@ETS.ES>
2021-07-20 12:55:46 +0200

    TST: add tests for io module

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,11 +1,21 @@
+import os
 import pytest
+from torchfitter import io
+from pathlib import Path
 
 
-@pytest.mark.xfail
+_path = Path(__file__).parent
+DATA_PATH = _path / "data"
+
+
 def test_save_pickle():
-    pass
+    a = [1, 2, 3]
+    io.save_pickle(obj=a, path=DATA_PATH / 'saving_test.pkl')
+    assert 'saving_test.pkl' in os.listdir(DATA_PATH), "Pickle not saved appropiately"
+    os.remove(DATA_PATH / 'saving_test.pkl')
 
 
-@pytest.mark.xfail
 def test_load_pickle():
-    pass
+    expected = [1, 2, 3]
+    obtained = io.load_pickle(DATA_PATH / 'pickle_test.pkl')
+    assert expected == obtained, "Pickle not loaded correctly"
